b7ebf89da416d67e8e2de61a8bd9b2d699d6ed4e
alexander-daniel <alex.vados@gmail.com>
2015-03-17 17:08:32 -0400

    silly

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ module.exports = function () {
 function floored () {
 
     function transformer(chunk, enc, callback) {
-        console.log(chunk.toString());
         this.push(Math.floor(chunk).toString() + '\n');
         callback();
     }
