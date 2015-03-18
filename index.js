fd1df0ea07f05b367619197f128b95fc34a7a415
alexander-daniel <alex.vados@gmail.com>
2015-03-17 21:52:46 -0400

    .

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ function FloorStream (opts) {
     if (!opts) opts = {};
     var separator = opts.separator || '';
 
-    separator = opts.separator;
-
     function transformer (chunk, enc, callback) {
         /*jshint validthis:true */
         this.push(Math.floor(chunk).toString());
         callback(null, separator);
     }
 
-    var floorStream = through(transformer);
-
-    return floorStream;
+    return through(transformer);
 }
 
 module.exports = FloorStream;
